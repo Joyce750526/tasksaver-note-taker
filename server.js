4530263c8c63358ad82710f8d9a128b773f7e00e
Joyce Chen <joyceideas@outlook.com>
2022-10-01 22:21:53 -0400

    added codes in server.js

--- server.js
+++ server.js
@@ -3,20 +3,37 @@ const path = require("path");
 
 const app = express();
 const PORT = 3001;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//TODO: GET * should return the index.html file.
-//refer to class activity 07-Ins_GET-Fetch/server.js for a get route to render a public html file.
+
 
 //TODO: GET /notes should return the notes.html file.
 //refer to class activity 07-Ins_GET-Fetch/server.js for a get route to render a public html file.
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 //TODO: GET /api/notes should read the db.json file and return all saved notes as JSON.
 //refer to class activity 28-Stu_Mini-Project/Main/routes/tips.js for a get route to render data from a file
+// GET Route for retrieving all the tips
+app.get('/api/notes', (req, res) => {
+  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 //TODO: POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 //refer to class activity 28-Stu_Mini-Project/Main/routes/tips.js for a POST route to take in a req.body and add to a .json file.
+
+//TODO: GET * should return the index.html file.
+//refer to class activity 07-Ins_GET-Fetch/server.js for a get route to render a public html file.
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);1
+
+
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
