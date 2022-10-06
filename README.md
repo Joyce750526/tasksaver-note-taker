0855c208b71332a84d7517c5ac99fbbf7c896186
Joyce Chen <joyceideas@outlook.com>
2022-10-06 19:00:49 -0400

    comment out the delete function for now

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # tasksaver-note-taker
+
+## Table of Contents
+* [License](#license)
+* [Purpose](#purpose)
+* [Usage](#usage)
+* [Knowledge](#knowledge)
+* [Demo Video](#demo-video)
+* [Deployed URL](#deployed-url)
+* [Questions](#questions)
+
+## License
 ![License:MIT](https://img.shields.io/badge/License-MIT-green)
 ![Heroku](https://img.shields.io/badge/Deploy%20to-Heroku-purple)
 ![Express.js](https://img.shields.io/badge/Express.js-Backend-yellowgreen)
 
 ## Purpose: 
-### Apply express.js to create backend plateform for user's to save notes.
-The main purpose is to save more time for the team on the project.
+### Apply express.js to create backend platform for user's to save notes.
+To create an application called `Tasksaver Note Taker` to be able to write and save notes for a small business owner to organize their business thoughts.
 
 ---
+## Usage
+This note taker application is designed to write and save notes for a small business. 
+How to use it- Clicking on top right-hand corner `+` to start typing your `Note Title` and then, typing your `Note Text` to in order to save your notes by clicking on top right-hand corner `Save Button` to save our notes.
+
+
 
 ## Knowledge: 
-
 * `JavaScript`
 * `Node.js`
 * `Express.js`
@@ -26,9 +41,9 @@ The main purpose is to save more time for the team on the project.
 
 
 ## Required to submit the following URL for review:
-
-* Github URL: 
-https://github.com/Joyce750526/tripsaver-weather-dashboard
+## Deployed URL
+* Github URL:
+https://github.com/Joyce750526/tasksaver-note-taker
 
 * Github Deployed Page: 
 https://joyce750526.github.io/tasksaver-note-taker/
@@ -37,5 +52,11 @@ https://joyce750526.github.io/tasksaver-note-taker/
 https://tasksaver-app.herokuapp.com/
 ---
 
+## Questions
+If you have any questions about this projects, please contact me!
+Email: [joyceideas@outlook.com](mailto:joyceideas@outlook.com)
+Github: [joyce750526](https://github.com/joyce750526)
+
+---
 Chao-Ying (Joyce) Chen
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {
   readFromFile,
   readAndAppend,
   writeToFile,
-} = require('./helper/filehelper.js');
+} = require("./helper/filehelper.js");
 
 // This sets up the Express App
 const app = express();
@@ -17,27 +17,26 @@ app.use(express.urlencoded({ extended: true }));
 // This is to Link to Assets
 app.use(express.static("public"));
 
-
 //TODO: GET /notes should return the notes.html file.
 //refer to class activity 07-Ins_GET-Fetch/server.js for a get route to render a public html file.
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 //TODO: GET /api/notes should read the db.json file and return all saved notes as JSON.
 //refer to class activity 28-Stu_Mini-Project/Main/routes/tips.js for a get route to render data from a file
 // GET Route for retrieving all the tips
-app.get('/api/notes', (req, res) => {
-  readFromFile('db/db.json').then((data) => res.json(JSON.parse(data)));
-});      
+app.get("/api/notes", (req, res) => {
+  readFromFile("db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 //TODO: POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 //refer to class activity 28-Stu_Mini-Project/Main/routes/tips.js for a POST route to take in a req.body and add to a .json file.
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
   console.log(req.body);
 
-  const {title, text } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
@@ -45,20 +44,36 @@ app.post('/api/notes', (req, res) => {
       text,
     };
 
-    readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, "./db/db.json");
     res.json(`Notes added successfully 🚀`);
   } else {
-    res.error('Error in adding notes');
+    res.error("Error in adding notes");
   }
+});
+
+// DELETE Route for a specific tip (needs to add delete button to make functional)
+app.delete("/api/notes/:id", (req, res) => {
+  const tipId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/tips.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
 });
 
 //TODO: GET * should return the index.html file.
 //refer to class activity 07-Ins_GET-Fetch/server.js for a get route to render a public html file.
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html')) 
-);1
-
-
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+1;
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
